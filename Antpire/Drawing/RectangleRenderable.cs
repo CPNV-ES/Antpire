6f2a71fc7e53b63de464396c96dd058b82223b94
Dimitri Imfeld <dimfeld@protonmail.ch>
2021-12-16 15:06:11 +0100

    Fix RectangleRenderable rotation

--- Antpire/Drawing/RectangleRenderable.cs
+++ Antpire/Drawing/RectangleRenderable.cs
@@ -6,28 +6,26 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
+using MonoGame.Extended.Shapes;
 
 namespace Antpire.Drawing {
     internal class RectangleRenderable : IRenderable {
         public Color Color = Color.White;
-        private Vector2[] points;
+        private Polygon polygon;
 
         public RectangleRenderable(Vector2 size, float rotation, Color color) {
             Color = color;
-            points = new Vector2[4];
-            points[0] = new Vector2(0, 0);
-            points[1] = new Vector2(size.X, 0);
-            points[2] = new Vector2(size.X, size.Y);
-            points[3] = new Vector2(0, size.Y);
-
-            for(int i = 0; i< 4; i++) {
-                points[i].X = (float)(points[i].X * Math.Cos(rotation) - points[i].Y * Math.Sin(rotation));
-                points[i].Y = (float)(points[i].X * Math.Sin(rotation) + points[i].Y * Math.Cos(rotation));
-            }
+            polygon = new Polygon(new Vector2[4] { 
+                new Vector2(0, 0),
+                new Vector2(size.X, 0),
+                new Vector2(size.X, size.Y),
+                new Vector2(0, size.Y),
+            });
+            polygon.Rotate(rotation);
         }
 
         public void Render(SpriteBatch spriteBatch, Point position) {
-            spriteBatch.DrawPolygon(new Vector2(position.X, position.Y), points, Color);
+            spriteBatch.DrawPolygon(new Vector2(position.X, position.Y), polygon, Color);
         }
     }
 }
