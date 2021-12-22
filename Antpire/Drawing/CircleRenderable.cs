7e407e41089be6c9eabcc2040e116c3d9bfabc5e
Dimitri Imfeld <dimfeld@protonmail.ch>
2021-12-22 12:05:19 +0100

    Can set CircleRenderable's thickness

--- Antpire/Drawing/CircleRenderable.cs
+++ Antpire/Drawing/CircleRenderable.cs
@@ -12,9 +12,10 @@ namespace Antpire.Drawing {
         public int Radius;
         public int Sides = 64;
         public Color Color = Color.White;
+        public float Thickness = 1.0f;
 
         public void Render(SpriteBatch spriteBatch, Point position) {
-            spriteBatch.DrawCircle(new CircleF { Position = position, Radius = Radius}, Sides, Color);
+            spriteBatch.DrawCircle(new CircleF { Position = position, Radius = Radius}, Sides, Color, Thickness);
         }
     }
 }
