dc044d14fae7d83743d8e6b8ae4f4153d7affac5
Dimitri Imfeld <dimfeld@protonmail.ch>
2021-12-22 12:04:45 +0100

    Implement RenderablesGroup to group renderables as one renderable

---
+++ Antpire/Drawing/RenderablesGroup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLinq;
+
+namespace Antpire.Drawing {
+    internal class RenderablesGroup : IRenderable {
+        public (IRenderable, Point)[] Children;
+
+        public void Render(SpriteBatch spriteBatch, Point position) {
+            Children.ForEach(child => child.Item1.Render(spriteBatch, position + child.Item2));
+        }
+    }
+}
