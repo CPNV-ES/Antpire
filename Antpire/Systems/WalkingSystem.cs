6b2364d23fb8753bc38383399f4d19707e618a15
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-02-04 17:31:48 +0100

    prepare Scouting method

--- Antpire/Systems/WalkingSystem.cs
+++ Antpire/Systems/WalkingSystem.cs
@@ -34,25 +34,60 @@ namespace Antpire.Systems
             {
                 var entity = simulationPosition.Get(entityId);
                 var insect = insectMapper.Get(entityId);
-
-                //insect.changeDestinationTo(new Vector2(100, 100));
-
-
                 Vector2 location = new Vector2(entity.Position.X, entity.Position.Y);
 
+
+                scouting(entity, insect, location, gameTime);
+
                 if (insect.shouldChangeDestination)
-                {
-                    insect.velocity = Vector2.Subtract(location / insect.destination, location);
-                    insect.velocity.Normalize();
-                    insect.velocity = Vector2.Multiply(insect.velocity, 1);
+                    changeDirection(insect, location);
 
-                    insect.shouldChangeDestination = false;
-                }
-
-                Vector2 newPosition = Vector2.Add(location, insect.velocity);
-
-                entity.Position = new Point((int)newPosition.X, (int)newPosition.Y);
+                walks(entity, insect, location);
             }
+        }
+
+        private void scouting(SimulationPosition entity, Insect insect, Vector2 location, GameTime gameTime)
+        {
+            /* Should I set this code inside the WalkingSystem or inside the Insect (or ant) component ?
+
+
+            */
+
+            /*
+            int counter = 1;
+            int limit = 50;
+            float countDuration = 2f; //every  2s.
+            float currentTime = 0f;
+
+            currentTime += (float)gameTime.ElapsedGameTime.TotalSeconds; //Time passed since last Update() 
+
+            if (currentTime >= countDuration)
+            {
+                counter++;
+                currentTime -= countDuration; // "use up" the time
+                                              //any actions to perform
+            }
+            if (counter >= limit)
+            {
+                insect.changeDestinationTo(new Vector2(50, 50));
+                counter = 0;//Reset the counter;
+                            //any actions to perform
+            }*/
+        }
+
+        private void walks(SimulationPosition entity, Insect insect, Vector2 location)
+        {
+            Vector2 newPosition = Vector2.Add(location, insect.velocity);
+            entity.Position = new Point((int)newPosition.X, (int)newPosition.Y);
+        }
+
+        private void changeDirection(Insect insect, Vector2 location)
+        {
+            insect.velocity = Vector2.Subtract(location / insect.destination, location);
+            insect.velocity.Normalize();
+            insect.velocity = Vector2.Multiply(insect.velocity, 1);
+
+            insect.shouldChangeDestination = false;
         }
     }
 }
