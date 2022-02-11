a1c3daee4c929051dcf4a7b7fb53e35a84a3a5e9
Dimitri Imfeld <dimfeld@protonmail.ch>
2022-02-11 09:52:16 +0100

    Add random trunks and bushes placement

--- Antpire/Utils/GardenGenerator.cs
+++ Antpire/Utils/GardenGenerator.cs
@@ -10,7 +10,7 @@ namespace Antpire.Utils;
 
 public class GardenGenerator {
     public struct GardenGenerationOptions {
-        public int ChunkSize = 1024;
+        public int ChunkSize = 768;
         public int Width = 3;
         public int Height = 3;
         public Range<int> RocksPerChunk = new(1, 1);
@@ -39,6 +39,8 @@ public class GardenGenerator {
         for(var y = 0; y < GenerationOptions.Height; y++) {
             for(var x = 0; x < GenerationOptions.Width; x++) {
                 PlaceRocksInChunk(new(x, y), world);
+                PlaceTrunksInChunk(new (x, y), world);
+                PlaceBushesInChunk(new (x, y), world);
 
                 var rectangle = world.CreateEntity();
                 rectangle.Attach(new SimulationPosition { Position = new Point(x * GenerationOptions.ChunkSize, y*GenerationOptions.ChunkSize), WorldSpace = WorldSpace.Garden });
@@ -142,6 +144,67 @@ public class GardenGenerator {
                     Color = Color.DarkGray,
                     Polygon = new Polygon(ShapeUtils.GenerateConvexPolygon(random.Next(GenerationOptions.RockVertices), random.Next(GenerationOptions.RockSize))),
                     Thickness = 5.0f
+                }
+            });
+        }
+    }
+    
+    private void PlaceTrunksInChunk(Point chunk, World world) {
+        for(var i = 0; i < random.Next(GenerationOptions.RocksPerChunk); i++) {
+            var pos = GetRandomPointInChunk(chunk); 
+            var trunk = world.CreateEntity();
+            var trunkWidth = random.Next(20, 30);
+            trunk.Attach(new SimulationPosition { Position = new Point(pos.X, pos.Y), WorldSpace = WorldSpace.Garden });
+            trunk.Attach(new Renderable {
+                RenderItem = new RectangleRenderable(
+                    size: new(trunkWidth, (float)(trunkWidth * (random.NextDouble()*3+2))), 
+                    rotation: (float)(random.NextDouble()*Math.PI*2), 
+                    color: Color.SaddleBrown,
+                    thickness: 30.0f
+                )
+            });
+        }
+    }
+
+    private void PlaceBushesInChunk(Point chunk, World world) {
+        for(var i = 0; i < random.Next(GenerationOptions.RocksPerChunk); i++) {
+            var pos = GetRandomPointInChunk(chunk); 
+            // Generate the bush's leaves
+            var leavesPositions = new List<Vector2>() { new(0, 0) };
+            var fruitsPositions = new List<Vector2>();
+
+            for (int b = 0; b < random.Next(6, 18); b++) {
+                leavesPositions.Add(ShapeUtils.GetRandomPointInCircle(50));
+                
+                // Generate fruits
+                for (int f = 0; f < random.Next(0, 5); f++) {
+                    fruitsPositions.Add(ShapeUtils.GetRandomPointInCircle(40) + leavesPositions.Last());                
+                }
+            }
+            
+            var bush = world.CreateEntity();
+            var fruits = world.CreateEntity();
+            bush.Attach(new SimulationPosition { Position = pos, WorldSpace= WorldSpace.Garden });
+            bush.Attach(new Renderable {
+                RenderItem = new RenderablesGroup {
+                    Children = leavesPositions.Select(x => 
+                        (
+                            new CircleRenderable { Sides = 32, Color = Color.DarkGreen, Thickness = 50.0f, Radius = 50 - (int)Math.Sqrt(x.X*x.X + x.Y*x.Y)/3 } as IRenderable,
+                            x.ToPoint()
+                        )
+                    ).ToArray()
+                }
+            });
+            var colors = new Color[] { Color.Red, Color.GreenYellow, Color.OrangeRed };
+            fruits.Attach(new SimulationPosition { Position = pos, WorldSpace = WorldSpace.Garden });
+            fruits.Attach(new Renderable {
+                RenderItem = new RenderablesGroup {
+                    Children = fruitsPositions.Select(x =>
+                        (
+                            new CircleRenderable { Sides = 32, Color = colors[random.Next(0, 3)], Thickness = 8.0f, Radius = random.Next(4, 8) } as IRenderable,
+                            x.ToPoint()
+                        )
+                    ).ToArray()
                 }
             });
         }
