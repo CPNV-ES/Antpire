f53bff9e80a4fbdef95bb918e9dfe346bbe8a53e
Dimitri Imfeld <dimfeld@protonmail.ch>
2022-02-20 17:26:35 +0100

    Fix culling with rotated SpriteRenderable
    Now checks if sprite's bounding square is within view region, so it may
    render rotated sprites that aren't visible but are still considered
    inside the view region.

--- Antpire/Drawing/SpriteRenderable.cs
+++ Antpire/Drawing/SpriteRenderable.cs
@@ -6,32 +6,24 @@ namespace Antpire.Drawing;
 internal class SpriteRenderable : IRenderable{
     private Texture2D texture;
     private Point size;
-
-    public SpriteRenderable(int size, Texture2D texture) {
-        this.texture = texture;
-        this.size = new Point(size, size);
-    }
-
-    public SpriteRenderable(Point size, Texture2D texture)
-    {
+    private int longestSide;
+    
+    public SpriteRenderable(Point size, Texture2D texture) {
         this.texture = texture;
         this.size = size;
+        this.longestSide = Math.Max(size.X, size.Y);
     }
+    
+    public SpriteRenderable(int size, Texture2D texture) : this(new Point(size, size), texture) { } 
 
     public void Render(SpriteBatch spriteBatch, Transform2 trans, Rectangle viewRegion) {
-        if(viewRegion.Intersects(new Rectangle { Location = trans.Position.ToPoint(), Width = size.X, Height = size.Y }))
-        {
-            if (trans.Rotation != 0) {
-                Vector2 location = trans.Position;
-                Rectangle sourceRectangle = new Rectangle(0, 0, texture.Width, texture.Height);
-                Vector2 origin = new Vector2(texture.Width / 2.0f, texture.Height / 2.0f);
-                float angle = trans.Rotation;
+        if(viewRegion.Intersects(new Rectangle { Location = new((int)trans.Position.X - longestSide/2, (int)trans.Position.Y - longestSide/2), Width = longestSide, Height = longestSide })) {
+            Vector2 location = trans.Position;
+            Rectangle sourceRectangle = new Rectangle(0, 0, texture.Width, texture.Height);
+            Vector2 origin = new Vector2(texture.Width / 2.0f, texture.Height / 2.0f);
+            float angle = trans.Rotation;
 
-                spriteBatch.Draw(texture, location, sourceRectangle, Color.White, angle+MathF.PI/2, origin, 0.5f, SpriteEffects.None, 1);
-            }
-            else {
-                spriteBatch.Draw(texture, new Rectangle(trans.Position.ToPoint(), size), Color.White);
-            }
+            spriteBatch.Draw(texture, location, sourceRectangle, Color.White, angle+MathF.PI/2, origin, 0.5f, SpriteEffects.None, 1);
         }
     }
 }
