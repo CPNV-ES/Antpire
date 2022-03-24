1fd6f7582757fabb2f0acfe969ab1aef0828f741
Dimitri Imfeld <dimfeld@protonmail.ch>
2022-03-24 14:40:51 +0100

    Make RectangleRenderable inherit PolygonRenderable

--- Antpire/Drawing/RectangleRenderable.cs
+++ Antpire/Drawing/RectangleRenderable.cs
@@ -5,19 +5,10 @@ using MonoGame.Extended.Shapes;
 
 namespace Antpire.Drawing; 
 
-internal class RectangleRenderable : IRenderable {
-    public Color Color = Color.White;
-    public Point BoundingBox => boundingBox;
-    public int Layer { get; init; }
-    public Polygon Polygon => polygon;
-    
-    private Polygon polygon;
-    private float thickness = 1.0f;
-    private Point boundingBox;
-    
-    public RectangleRenderable(Vector2 size, float rotation, Color color, float thickness = 1.0f) {
+internal class RectangleRenderable : PolygonRenderable {
+    public RectangleRenderable(Vector2 size, float rotation, Color color, float thickness = 1.0f) : base() {
         Color = color;
-        polygon = new Polygon(new[] { 
+        var polygon = new Polygon(new[] { 
             new Vector2(0, 0),
             new Vector2(size.X, 0),
             new Vector2(size.X, size.Y),
@@ -25,13 +16,7 @@ internal class RectangleRenderable : IRenderable {
         });
         
         polygon.Rotate(rotation);
-        
-        this.thickness = thickness;
-        var br = polygon.BoundingRectangle.ToRectangle();
-        boundingBox = new(br.Width * 2, br.Height * 2); // TODO: Shouldn't be times two, but the BoundingRectangle can't account for vertices with negative positions. Should bring all vertices to have X and Y be positive.
-    }
-    
-    public void Render(DrawBatch drawBatch, Transform2 trans) {
-        drawBatch.GetShapeDrawBatch((DrawBatch.Layer)3).FillPath(new SolidColorBrush(Color), polygon.Vertices.Select(x => x + trans.Position).ToList());
+
+        base.Polygon = new Polygon(polygon.Vertices.Reverse());
     }
 }
