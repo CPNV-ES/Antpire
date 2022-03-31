e22fea214ee37dcd56e0402995d4998e694bca33
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-03-31 14:22:21 +0200

    Create LoggerSystem.cs

---
+++ Antpire/Systems/LoggerSystem.cs
@@ -0,0 +1,30 @@
+﻿using MonoGame.Extended.Entities;
+using MonoGame.Extended.Entities.Systems;
+using Antpire.Components;
+using Antpire.Screens;
+using MonoGame.Extended;
+
+namespace Antpire.Systems; 
+
+internal class LoggerSystem : EntityUpdateSystem
+{
+    private ComponentMapper<Ant> antMapper;
+    private SimulationState simState;
+
+
+    public LoggerSystem(SimulationState simState) : base(Aspect.All(typeof(Ant))) {
+        this.simState = simState;
+    }
+
+    public override void Initialize(IComponentMapperService mapperService) {
+        antMapper = mapperService.GetMapper<Ant>();
+    }
+
+    public override void Update(GameTime gameTime) {
+        foreach (var entityId in ActiveEntities) {
+            var ant = antMapper.Get(entityId);
+
+
+        }
+    }
+}
