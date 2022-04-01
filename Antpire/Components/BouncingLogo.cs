e4787c3cccf01f3cd5b27f42f3ccc63b977d2750
Dimitri Imfeld <dimfeld@protonmail.ch>
2022-04-01 13:20:18 +0200

    Remove bounding logo related stuff

--- Antpire/Components/BouncingLogo.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-
-namespace Antpire.Components; 
-
-internal class BouncingLogo {
-    public Vector2 Velocity;
-    public Texture2D Sprite;
-    public Vector2 Size;
-}
